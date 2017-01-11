a71912d488c3ee1a3b0f89bf9f59f4a3d9bcf9fb
Alain Benbassat <alain@businessandcode.eu>
2017-01-11 17:28:09 +0100

    first version

---
+++ CRM/Bemasreporting/Form/Report/EventDashboard.php
@@ -0,0 +1,182 @@
+<?php
+
+class CRM_Bemasreporting_Form_Report_EventDashboard extends CRM_Report_Form {
+  protected $_addressField = FALSE;
+  protected $_emailField = FALSE;
+  protected $_summary = NULL;
+  protected $_exposeContactID = FALSE;
+
+  function __construct() {
+    $this->_columns = array(
+      'civicrm_event' => array(
+        'dao' => 'CRM_Event_DAO_Event',
+        'fields' => array(
+          'id' => array(
+            'title' => 'event_id',
+            'no_display' => TRUE,
+            'required' => TRUE,
+          ),
+          'start_date' => array(
+            'title' => 'Datum',
+            'required' => TRUE,
+            'dbAlias' => 'date_format(start_date, \'%Y-%m-%d\')',
+          ),
+          'title' => array(
+            'title' => ts('Activiteit'),
+            'required' => TRUE,
+          ),
+          'registered_attended' => array(
+            'title' => ts('Ingeschreven/Deelgenomen'),
+            'required' => TRUE,
+            'dbAlias' => '-1',
+          ),
+          'cancel_noshow' => array(
+            'title' => ts('Niet gekomen/geannuleerd'),
+            'required' => TRUE,
+            'dbAlias' => '-1',
+          ),
+          'invoiced' => array(
+            'title' => ts('Gefactureerd'),
+            'required' => TRUE,
+            'dbAlias' => '-1',
+          ),
+        ),
+        'filters' => array(
+          'start_date' => array(
+            'title' => 'Datum',
+            'type' => CRM_Utils_Type::T_DATE,
+            'default' => array(
+              'from' => date('m/d/Y', time() - (86400 * 30)), // current date - 30 days
+              'to' => date('m/d/Y', time() + (86400 * 45)), // current date + 45 days
+            ),
+          ),
+        ),
+      ),
+    );
+
+    parent::__construct();
+  }
+
+  function preProcess() {
+    $this->assign('reportTitle', ts('Activiteiten dashboard'));
+    parent::preProcess();
+  }
+
+  function select() {
+    $select = $this->_columnHeaders = array();
+
+    foreach ($this->_columns as $tableName => $table) {
+      if (array_key_exists('fields', $table)) {
+        foreach ($table['fields'] as $fieldName => $field) {
+          if (CRM_Utils_Array::value('required', $field) ||
+            CRM_Utils_Array::value($fieldName, $this->_params['fields'])
+          ) {
+            if ($tableName == 'civicrm_address') {
+              $this->_addressField = TRUE;
+            }
+            elseif ($tableName == 'civicrm_email') {
+              $this->_emailField = TRUE;
+            }
+            $select[] = "{$field['dbAlias']} as {$tableName}_{$fieldName}";
+            $this->_columnHeaders["{$tableName}_{$fieldName}"]['title'] = $field['title'];
+            $this->_columnHeaders["{$tableName}_{$fieldName}"]['type'] = CRM_Utils_Array::value('type', $field);
+          }
+        }
+      }
+    }
+
+    $this->_select = "SELECT " . implode(', ', $select) . " ";
+  }
+
+  function from() {
+    $this->_from = " FROM  civicrm_event {$this->_aliases['civicrm_event']}  ";
+  }
+
+  function where() {
+    $clauses = array();
+    foreach ($this->_columns as $tableName => $table) {
+      if (array_key_exists('filters', $table)) {
+        foreach ($table['filters'] as $fieldName => $field) {
+          $clause = NULL;
+          if (CRM_Utils_Array::value('operatorType', $field) & CRM_Utils_Type::T_DATE) {
+            $relative = CRM_Utils_Array::value("{$fieldName}_relative", $this->_params);
+            $from     = CRM_Utils_Array::value("{$fieldName}_from", $this->_params);
+            $to       = CRM_Utils_Array::value("{$fieldName}_to", $this->_params);
+
+            $clause = $this->dateClause($field['name'], $relative, $from, $to, $field['type']);
+          }
+          else {
+            $op = CRM_Utils_Array::value("{$fieldName}_op", $this->_params);
+            if ($op) {
+              $clause = $this->whereClause($field,
+                $op,
+                CRM_Utils_Array::value("{$fieldName}_value", $this->_params),
+                CRM_Utils_Array::value("{$fieldName}_min", $this->_params),
+                CRM_Utils_Array::value("{$fieldName}_max", $this->_params)
+              );
+            }
+          }
+
+          if (!empty($clause)) {
+            $clauses[] = $clause;
+          }
+        }
+      }
+    }
+
+    if (empty($clauses)) {
+      $this->_where = "WHERE ( 1 ) ";
+    }
+    else {
+      $this->_where = "WHERE " . implode(' AND ', $clauses);
+    }
+
+  }
+
+  function orderBy() {
+    $this->_orderBy = " ORDER BY {$this->_aliases['civicrm_event']}.start_date DESC";
+  }
+
+  function postProcess() {
+    $this->beginPostProcess();
+
+    $sql = $this->buildQuery(TRUE);
+
+    $rows = array();
+    $this->buildRows($sql, $rows);
+
+    $this->formatDisplay($rows);
+    $this->doTemplateAssignment($rows);
+    $this->endPostProcess($rows);
+  }
+
+  function alterDisplay(&$rows) {
+    foreach ($rows as $rowNum => $row) {
+      // change title in a hyperlink
+      $eventLink = CRM_Utils_System::baseURL() . 'civicrm/event/manage/settings?reset=1&action=update&id=' . $row['civicrm_event_id'];
+      $url = "<a href=\"$eventLink\">{$row['civicrm_event_title']}</a>";
+      $rows[$rowNum]['civicrm_event_title'] = $url;
+
+      // count the registered participants (= everything but no show and cancel
+      $statusIDs = '3,4';
+      $sql = "select count(*) from civicrm_participant p inner join civicrm_contact c on c.id = p.contact_id where c.is_deleted = 0 and 
+        p.event_id = " . $row['civicrm_event_id'] . " and p.role_id = 1 and p.status_id not in (" . $statusIDs . ")";
+      $count = CRM_Core_DAO::singleValueQuery($sql);
+      $rows[$rowNum]['civicrm_event_registered_attended'] = $count;
+
+      // count the canceled and no-show participants
+      $statusIDs = '3,4';
+      $sql = "select count(*) from civicrm_participant p inner join civicrm_contact c on c.id = p.contact_id where c.is_deleted = 0 and 
+        p.event_id = " . $row['civicrm_event_id'] . " and p.role_id = 1 and p.status_id in (" . $statusIDs . ")";
+      $count = CRM_Core_DAO::singleValueQuery($sql);
+      $rows[$rowNum]['civicrm_event_cancel_noshow'] = $count;
+
+      // count the invoiced participants
+      $statusIDs = '16';
+      $sql = "select count(*) from civicrm_participant p inner join civicrm_contact c on c.id = p.contact_id where c.is_deleted = 0 and 
+        p.event_id = " . $row['civicrm_event_id'] . " and p.role_id = 1 and p.status_id = " . $statusIDs;
+      $count = CRM_Core_DAO::singleValueQuery($sql);
+      $rows[$rowNum]['civicrm_event_invoiced'] = $count;
+    }
+  }
+}
---
+++ bemasreporting.php
@@ -0,0 +1,155 @@
+<?php
+
+require_once 'bemasreporting.civix.php';
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function bemasreporting_civicrm_config(&$config) {
+  _bemasreporting_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @param array $files
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function bemasreporting_civicrm_xmlMenu(&$files) {
+  _bemasreporting_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function bemasreporting_civicrm_install() {
+  _bemasreporting_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function bemasreporting_civicrm_uninstall() {
+  _bemasreporting_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function bemasreporting_civicrm_enable() {
+  _bemasreporting_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function bemasreporting_civicrm_disable() {
+  _bemasreporting_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed
+ *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function bemasreporting_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _bemasreporting_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function bemasreporting_civicrm_managed(&$entities) {
+  _bemasreporting_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * @param array $caseTypes
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function bemasreporting_civicrm_caseTypes(&$caseTypes) {
+  _bemasreporting_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function bemasreporting_civicrm_angularModules(&$angularModules) {
+_bemasreporting_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function bemasreporting_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _bemasreporting_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Functions below this ship commented out. Uncomment as required.
+ *
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function bemasreporting_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ *
+function bemasreporting_civicrm_navigationMenu(&$menu) {
+  _bemasreporting_civix_insert_navigation_menu($menu, NULL, array(
+    'label' => ts('The Page', array('domain' => 'eu.businessandcode.bemasreporting')),
+    'name' => 'the_page',
+    'url' => 'civicrm/the-page',
+    'permission' => 'access CiviReport,access CiviContribute',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _bemasreporting_civix_navigationMenu($menu);
+} // */
