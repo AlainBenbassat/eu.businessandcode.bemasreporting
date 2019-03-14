f085297d99d0a4930803bc2bbf13d4bad26f40cd
Alain Benbassat <alain@businessandcode.eu>
2019-03-14 14:18:10 +0100

    adds inconsistencies custom search

---
+++ CRM/Bemasreporting/Form/Search/Inconsistencies.php
@@ -0,0 +1,91 @@
+<?php
+use CRM_Bemasreporting_ExtensionUtil as E;
+
+class BemasInconsistenciesQuery {
+  public $label;
+  public $index;
+  public $from;
+  public $where;
+}
+
+class CRM_Bemasreporting_Form_Search_Inconsistencies extends CRM_Contact_Form_Search_Custom_Base implements CRM_Contact_Form_Search_Interface {
+  private $queries = [];
+  private $queriesRadioButtons = [];
+
+  function __construct(&$formValues) {
+    $this->addQueries();
+
+    parent::__construct($formValues);
+  }
+
+  function buildForm(&$form) {
+    CRM_Utils_System::setTitle('Fouten in de database');
+
+    $form->addCheckBox('queryFilter', 'Te controleren:', $this->queriesRadioButtons, NULL, NULL, TRUE);
+
+    $form->assign('elements', ['queryFilter']);
+  }
+
+  function &columns() {
+    // return by reference
+    $columns = array(
+      E::ts('Contact Id') => 'contact_id',
+      E::ts('Contact Type') => 'contact_type',
+      E::ts('Name') => 'sort_name',
+      E::ts('State') => 'state_province',
+    );
+    return $columns;
+  }
+
+  function all($offset = 0, $rowcount = 0, $sort = NULL, $includeContactIDs = FALSE, $justIDs = FALSE) {
+    $sql = $this->sql($this->select(), $offset, $rowcount, $sort, $includeContactIDs, NULL);
+    return $sql;
+  }
+
+  function select() {
+    $select = "
+      contact_a.id as contact_id
+      , contact_a.sort_name as sort_name
+    ";
+
+    return $select;
+  }
+
+  function from() {
+    $values = $this->_formValues;
+    $from = $this->queries[$values['queryFilter']]->from;
+
+    return $from;
+  }
+
+  function where($includeContactIDs = FALSE) {
+    $whereParams = [];
+
+    $values = $this->_formValues;
+    $where = $this->queries[$values['queryFilter']]->where;
+
+    return $this->whereClause($where, $whereParams);
+  }
+
+  function templateFile() {
+    return 'CRM/Contact/Form/Search/Custom.tpl';
+  }
+
+  function addQueries() {
+    $index = 0;
+
+    // contacten zonder prefix_id
+    $q = new BemasInconsistenciesQuery();
+    $q->label = 'Contacten zonder voorvoegsel (Dhr./Mevr.)';
+    $q->index = $index;
+    $q->from = "civicrm_contact contact_a";
+    $q->where = "
+      contact_a.prefix_id IS NULL
+      and contact_a.contact_type = 'Individual'
+      and contact_a.is_deleted = 0
+    ";
+    $this->queries[$index] = $q;
+    $this->queriesRadioButtons[$q->label] = $q->index;
+    $index++;
+  }
+}
