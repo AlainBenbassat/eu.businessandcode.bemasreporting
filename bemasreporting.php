91130ec9a213a61e75e10f3ea19b1c25618f63e7
Alain Benbassat <alain@businessandcode.eu>
2024-10-20 18:20:11 +0200

    upgrade

--- bemasreporting.php
+++ bemasreporting.php
@@ -42,46 +42,12 @@ function bemasreporting_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function bemasreporting_civicrm_uninstall() {
-  _bemasreporting_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function bemasreporting_civicrm_enable() {
   _bemasreporting_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function bemasreporting_civicrm_disable() {
-  _bemasreporting_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function bemasreporting_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _bemasreporting_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -112,21 +78,3 @@ function bemasreporting_civicrm_navigationMenu(&$menu) {
   ));
   _bemasreporting_civix_navigationMenu($menu);
 } // */
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function bemasreporting_civicrm_postInstall() {
-  _bemasreporting_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function bemasreporting_civicrm_entityTypes(&$entityTypes) {
-  _bemasreporting_civix_civicrm_entityTypes($entityTypes);
-}
